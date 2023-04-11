1bb0b0564e5e739efd34234a9214ea4b6616659a
titi <sackotiti@gmail.com>
2023-04-11 16:32:02 +0100

    declarations

--- declarations.js
+++ declarations.js
@@ -1,4 +1,4 @@
-const escapeStr = "`\/\"'";
+const escapeStr = "`\\/\"'";
 const arr = [4,'2'];
 const obj = {str:'y', num:7, bool:true, undef:undefined}
 const nested = {arr:[4,undefined,'2'], obj:{str:'h', num:1,bool:true}}
