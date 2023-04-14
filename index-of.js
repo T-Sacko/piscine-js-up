e35caffaca2edcdfe565ad8c86680e58894cd382
titi <sackotiti@gmail.com>
2023-04-14 16:13:56 +0100

    declarations

---
+++ index-of.js
@@ -0,0 +1,24 @@
+function indexOf(arr, val, optIndex) {
+    for (let i = optIndex || 0; i < arr.length; i++) {
+      if (arr[i] === val) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  function lastIndexOf(arr, val, optIndex) {
+    for (let i = optIndex || arr.length - 1; i >= 0; i--) {
+      if (arr[i] === val) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  function includes(arr, val, optIndex) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === val) {
+        return true;
+      }
+    }
+    return false;
+  }
