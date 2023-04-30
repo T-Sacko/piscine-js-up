1b59baa3e0d9b9c89817037c90bfbc5601273608
titi <sackotiti@gmail.com>
2023-04-30 20:12:49 +0100

    muts

--- more-or-less.js
+++ more-or-less.js
@@ -1,15 +1,15 @@
 function more(x) {
-   return x+1;
+   x++;
 }
 
 function less(x) {
-   return x-1;
+    x--;
 }
 
 function add(x, y) {
-   return x+y;
+    x+=y;
 }
 
 function sub(x,y) {
-   return x-y;
+   x-=y;
 }
