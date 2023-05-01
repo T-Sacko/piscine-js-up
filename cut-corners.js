e4754c814ea3c20e441788f96b548d27b70fa32c
titi <sackotiti@gmail.com>
2023-05-01 11:32:35 +0100

    muts

--- cut-corners.js
+++ cut-corners.js
@@ -1,16 +1,26 @@
 function round(num) {
-  return num >= 0 ? Math.floor(num + 0.5) : Math.ceil(num - 0.5);
+  return num >= 0 ? integral(num + 0.5) : integral(num - 0.5);
 }
 
 function ceil(num) {
-  return num > 0 ? Math.floor(num + 1) : Math.ceil(num);
+  return num > 0 ? integral(num + 1) : integral(num);
 }
 
 function floor(num) {
-  return num >= 0 ? Math.floor(num) : Math.floor(num);
+  return num >= 0 ? integral(num) : integral(num - 1);
 }
 
 function trunc(num) {
-  return num >= 0 ? Math.floor(num) : Math.ceil(num);
+  return num >= 0 ? integral(num) : integral(num - 1);
 }
 
+function integral(num) {
+  let intPart = 0;
+  let sign = num >= 0 ? 1 : -1;
+  num = num * sign;
+  while (num >= 1) {
+    num -= 1;
+    intPart += 1;
+  }
+  return intPart * sign;
+}
