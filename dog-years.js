28fc3a591d4d49198da06c57ac8140cfbd1e82fb
titi <sackotiti@gmail.com>
2023-05-01 11:48:27 +0100

    muts

--- dog-years.js
+++ dog-years.js
@@ -1,28 +1,15 @@
-function dogYears(plan, age) {
+function dogYears(planet, age) {
+  const planets = {
+      earth: 1,
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132,
+  };
+  let earthAge = (age / 31557600) * 7;
 
-  let secs = 31557600
-  let mult = 7
-
-  const periods = {
-    earth: 1,
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132,
-  }
-
-  const eyears = age/secs
-  let planet=plan.toLowerCase()
-  let planyears = eyears/periods[planet];
-  planyears=planyears*mult
-
-  return parseFloat(planyears.toFixed(2));
-
-  
+  return Number(Number(earthAge / planets[planet]).toFixed(2));
 }
-
-const yhh = dog_years('earth',1000000000)
-console.log(yhh)
