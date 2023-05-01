812fccd2ae4b878d86e12e47ac362846b241648c
titi <sackotiti@gmail.com>
2023-05-01 11:41:57 +0100

    muts

--- cut-corners.js
+++ cut-corners.js
@@ -1,3 +1,14 @@
+function integral(num) {
+  let intPart = 0;
+  let sign = num >= 0 ? 1 : -1;
+  num = num * sign;
+  while (num >= 1) {
+    num -= 1;
+    intPart += 1;
+  }
+  return intPart * sign;
+}
+
 function round(num) {
   return num >= 0 ? integral(num + 0.5) : integral(num - 0.5);
 }
@@ -14,14 +25,4 @@ function trunc(num) {
   return num >= 0 ? integral(num) : integral(num);
 }
 
-function integral(num) {
-  let intPart = 0;
-  let sign = num >= 0 ? 1 : -1;
-  num = num * sign;
-  while (num >= 1) {
-    num -= 1;
-    intPart += 1;
-  }
-  return intPart * sign;
-}
 
