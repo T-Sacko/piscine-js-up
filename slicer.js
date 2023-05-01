dd61725ab350b2f01885c35f45cd241b7c40bdf4
titi <sackotiti@gmail.com>
2023-05-01 10:16:45 +0100

    muts

--- slicer.js
+++ slicer.js
@@ -1,27 +1,22 @@
 function slice(input, start, end) {
-  const isString = typeof input === 'string'
-  const result = isString ? '' : [];
+  const isString = typeof input === 'string';
+  let result = isString ? '' : [];
 
-  if (start < 0){
-    start +=input.length
-  }else if(start===undefined) {
-    start=0
+  if (start < 0) {
+    start += input.length;
+  } else if (start === undefined) {
+    start = 0;
   }
 
-  if (end<0){
-    end+=input.length
-  } else if (end === undefined){
-    end = input.length
+  if (end < 0) {
+    end += input.length;
+  } else if (end === undefined) {
+    end = input.length;
   }
 
   for (let i = start; i < end && i < input.length; i++) {
-      isString ? (result+=input[i]) : result.push(input[i])
+    isString ? (result += input[i]) : result.push(input[i]);
   }
-  
-  return result
 
-  
-  
+  return result;
 }
-const ting = ['one','two', 'three', 'four', 'five']
-console.log(slice(ting,1))
