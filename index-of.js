b9136651888dde8d01bda9344c6d9b039662bb72
titi <sackotiti@gmail.com>
2023-05-01 10:07:20 +0100

    muts

--- index-of.js
+++ index-of.js
@@ -5,6 +5,7 @@ function indexOf(arr, val, optIndex) {
       }
     }
     return -1;
+    return arr[i] === val 
   }
   function lastIndexOf(arr, val, optIndex) {
     for (let i = optIndex || arr.length - 1; i >= 0; i--) {
