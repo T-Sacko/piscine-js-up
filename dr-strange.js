fccb3d0647461c7d93a6598119c5b35fca9e2ef7
titi <sackotiti@gmail.com>
2023-05-02 17:53:06 +0100

    sr

--- dr-strange.js
+++ dr-strange.js
@@ -1,24 +1,40 @@
 function addWeek(date) {
-    const secondWeek = {
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday",
-        6: "Sunday",
-        7: "secondMonday",
-        8: "secondTuesday",
-        9: "secondWednesday",
-        10: "secondThursday",
-        11: "secondFriday",
-        12: "secondSaturday",
-        13: "secondSunday",
-    };
-    let zTime = date.getTime() + 62135596800000;
-    return secondWeek[(zTime / 86400000) % 14];
+    const secondWeek = [
+         "Monday",
+         "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+         "Saturday",
+         "Sunday",
+        "secondMonday",
+        "secondTuesday",
+        "secondWednesday",
+         "secondThursday",
+        "secondFriday",
+         "secondSaturday",
+         "secondSunday",
+    ];
+  
+    const epoch = new Date("0001-01-01")
+    const daysSince = Math.floor((date - epoch) / (1000*60*60*24))
+    const index = daysSince %14
+      return secondWeek[index] ;
+    
 }
 
-function timeTravel(date) {
-    return new Date(date.date.setHours(date.hour, date.minute, date.second));
+const ting ={
+    date: new Date(2020,00,20),
+    hour: 20,
+    minute: 2,
+    second: 300
 }
+
+function timeTravel({date, hour, minute, second}) {
+
+    return new Date(date.setHours(hour,minute,second)).toString()
+}
+
+console.log(timeTravel(ting))
+
+console.log(addWeek(0001-01-01))
