8c105a0c226509c2fb90343f5345c9d27274037b
titi <sackotiti@gmail.com>
2023-05-02 19:00:30 +0100

    sr

--- date-is.js
+++ date-is.js
@@ -1,22 +1,43 @@
 function isValid(date) {
-    return !isNaN(date.getTime());
-  }
-  
-  function isAfter(date1, date2) {
-    return date1 > date2;
-  }
-  
-  function isBefore(date1, date2) {
-    return date1 < date2;
-  }
-  
-  function isFuture(date) {
-    const now = new Date();
-    return isAfter(date, now);
-  }
-  
-  function isPast(date) {
-    const now = new Date();
-    return isBefore(date, now);
-  }
-  
+    if (new Date(date).toString() === "Invalid Date") {
+        return false;
+    }
+    if (!(date instanceof Date) && typeof date !== "number") {
+        return false;
+    }
+    return true;
+}
+
+function isAfter(d1, d2) {
+    if (d1 > d2) {
+        return true;
+    }
+    return false;
+}
+
+function isBefore(d1, d2) {
+    if (d1 < d2) {
+        return true;
+    }
+    return false;
+}
+
+function isFuture(date) {
+    if (!isValid(date)) {
+        return false;
+    }
+    if (new Date(date).getTime() > Date.now()) {
+        return true;
+    }
+    return false;
+}
+
+function isPast(date) {
+    if (!isValid(date)) {
+        return false;
+    }
+    if (new Date(date).getTime() < Date.now()) {
+        return true;
+    }
+    return false;
+}
--- dr-strange.js
+++ dr-strange.js
@@ -1,30 +1,24 @@
 function addWeek(date) {
-    const secondWeek = [
-         "Monday",
-         "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-         "Saturday",
-         "Sunday",
-        "secondMonday",
-        "secondTuesday",
-        "secondWednesday",
-         "secondThursday",
-        "secondFriday",
-         "secondSaturday",
-         "secondSunday",
-    ];
-  
-    const epoch = new Date("0001-01-01")
-    const daysSince = Math.floor((date - epoch) / (1000*60*60*24))
-    const index = daysSince %14
-      return secondWeek[index] ;
-    
+    const secondWeek = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday",
+        7: "secondMonday",
+        8: "secondTuesday",
+        9: "secondWednesday",
+        10: "secondThursday",
+        11: "secondFriday",
+        12: "secondSaturday",
+        13: "secondSunday",
+    };
+    let zTime = date.getTime() + 62135596800000;
+    return secondWeek[(zTime / 86400000) % 14];
 }
 
-function timeTravel({date, hour, minute, second}) {
-
-    return new Date(date.setHours(hour,minute,second)).toString()
+function timeTravel(date) {
+    return new Date(date.date.setHours(date.hour, date.minute, date.second));
 }
-
