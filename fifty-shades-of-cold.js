5bab3f87d12c48a3ced94272bf356d67a1c543bc
titi <sackotiti@gmail.com>
2023-05-03 18:02:01 +0100

    map

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,38 +1,33 @@
 import { colors } from "./fifty-shades-of-cold.data.js";
 
 function generateClasses() {
-
-    const head = document.querySelector('head')
-    const style = document.createElement('style')
-
-    colors.forEach(color => {
-        style.innerHTML += `.${color}{\n  background: ${color};\n\n`
+    const head = document.getElementsByTagName("head")[0];
+    const style = document.createElement("style");
+    colors.forEach((color) => {
+        style.innerHTML += `.${color} {\n  background: ${color};\n }\n\n`;
     });
-
-    head.appendChild(style)
-
-
+    console.log(style.innerHTML);
+    head.appendChild(style);
 }
 
 function generateColdShades() {
-    colors.forEach(color => {
-        const name = color.toLowerCase();
-        if (name.includes('aqua') || name.includes('blue') || name.includes('turquoise') || name.includes('green') || name.includes('cyan') || name.includes('navy') || name.includes('purple')) {
-        const div = document.createElement('div')
-        div.className(color)
-        div.textContent(color)
-        body.appendChild(div);
+    const body = document.getElementsByTagName("body")[0];
+    colors.forEach((color) => {
+        if (
+            color.match(/(aqua|blue|turquoise|green|cyan|navy|purple)/) !== null
+        ) {
+            const div = document.createElement("div");
+            div.classList.add(color);
+            div.innerHTML = color;
+            body.appendChild(div);
         }
-    })
+    });
 }
 
 function choseShade(shade) {
-
-    const divs = document.querySelectorAll('div');
-    divs.forEach(div => {
-        if (div.className!=shade) {
-            div.classList.replace(classList[0],shade)
-        }
-    })
-
+    document.querySelectorAll("div").forEach((div) => {
+        div.className = shade;
+    });
 }
+
+export { generateClasses, generateColdShades, choseShade };
