a34af7d882a28d242c2da554ceae9b1fda556679
titi <sackotiti@gmail.com>
2023-05-03 18:00:28 +0100

    map

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,38 @@
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+function generateClasses() {
+
+    const head = document.querySelector('head')
+    const style = document.createElement('style')
+
+    colors.forEach(color => {
+        style.innerHTML += `.${color}{\n  background: ${color};\n\n`
+    });
+
+    head.appendChild(style)
+
+
+}
+
+function generateColdShades() {
+    colors.forEach(color => {
+        const name = color.toLowerCase();
+        if (name.includes('aqua') || name.includes('blue') || name.includes('turquoise') || name.includes('green') || name.includes('cyan') || name.includes('navy') || name.includes('purple')) {
+        const div = document.createElement('div')
+        div.className(color)
+        div.textContent(color)
+        body.appendChild(div);
+        }
+    })
+}
+
+function choseShade(shade) {
+
+    const divs = document.querySelectorAll('div');
+    divs.forEach(div => {
+        if (div.className!=shade) {
+            div.classList.replace(classList[0],shade)
+        }
+    })
+
+}
---
+++ harder-bigger-bolder-stronger copy.js
@@ -0,0 +1,20 @@
+export function generateLetters() {
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    for (let i = 0; i < 120; i++) {
+        // create div element
+        const letterdiv = document.createElement('div')
+        // get random number from uppercase alphabet
+        const letter = alphabet[Math.floor(Math.random() * 26)]
+        // add letter to the div
+        letterdiv.textContent = letter
+        // det font size dynamically
+        letterdiv.style.fontSize = `${11 + i}`
+        // set font weight dynamically
+        letterdiv.style.fontWeight = i < 40 ? '300' : (i < 80 ? '400' : '600');
+        // add div to html body
+        document.body.appendChild(letterdiv);
+    }
+}
+
