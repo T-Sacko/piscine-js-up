8cd05130cb8a1a888b81ac02be5fad8a8c04500b
titi <sackotiti@gmail.com>
2023-05-06 14:14:35 +0100

    async

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,8 +1,10 @@
 const words = input.trim().split(' ');
-if (words.length === 1) {
-  const word = words[0];
+const result = [];
+for (let i = 0; i < words.length; i++) {
+  const word = words[i];
   const halfLength = Math.ceil(word.length / 2);
   const firstHalf = word.slice(0, halfLength);
   const secondHalf = word.slice(halfLength);
-  return secondHalf + firstHalf;
+  result.push(secondHalf + firstHalf);
 }
+return result.join(' ');
