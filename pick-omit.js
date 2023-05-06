04db5e7a02c286702a23c333726aa4bd02ae82aa
titi <sackotiti@gmail.com>
2023-05-06 16:57:07 +0100

    obj

--- pick-omit.js
+++ pick-omit.js
@@ -1,34 +1,71 @@
 function pick(obj, keys) {
-    const picked = {};
-    for (let key in obj) {
-        let regexp = new RegExp(`^${key}$`);
-        if (typeof keys === "string") {
-            if (keys.match(regexp)) {
-                picked[key] = obj[key];
-            }
-        } else {
-            if (keys.includes(key)) {
-                picked[key] = obj[key];
-            }
-        }
+    const result={}
+
+    if (typeof keys === 'string') {
+        keys=[keys]
     }
-    return picked;
+
+    keys.forEach(key => {
+        if (obj.hasOwnProperty(key)) {
+            result[key] = obj[key]
+        }
+    });
+
+    return result
 }
 
 function omit(obj, keys) {
-    const omitted = {};
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key) === false) continue;
-        let regexp = new RegExp(`^${key}$`);
-        if (typeof keys === "string") {
-            if (!keys.match(regexp)) {
-                omitted[key] = obj[key];
-            }
-        } else {
-            if (!keys.includes(key)) {
-                omitted[key] = obj[key];
-            }
-        }
+    const result={}
+
+    if(typeof keys === 'string') {
+        keys = [keys]
     }
-    return omitted;
+
+    for (let key in obj){
+        !keys.includes(key) && obj.hasOwnProperty(key) ? result[key] = obj[key] : null
+    }
+
 }
+
+const obj = { a: 1, b: 2, c: 3 };
+const keys = ['a', 'c'];
+
+(() => {
+  const result = pick(obj, keys);
+  console.log(result);
+})();
+
+// function pick(obj, keys) {
+//     const picked = {};
+//     for (let key in obj) {
+//         let regexp = new RegExp(`^${key}$`);
+//         if (typeof keys === "string") {
+//             if (keys.match(regexp)) {
+//                 picked[key] = obj[key];
+//             }
+//         } else {
+//             if (keys.includes(key)) {
+//                 picked[key] = obj[key];
+//             }
+//         }
+//     }
+//     return picked;
+// }
+
+// function omit(obj, keys) {
+//     const omitted = {};
+//     for (let key in obj) {
+//         if (obj.hasOwnProperty(key) === false) continue;
+//         let regexp = new RegExp(`^${key}$`);
+//         if (typeof keys === "string") {
+//             if (!keys.match(regexp)) {
+//                 omitted[key] = obj[key];
+//             }
+//         } else {
+//             if (!keys.includes(key)) {
+//                 omitted[key] = obj[key];
+//             }
+//         }
+//     }
+//     return omitted;
+// }
