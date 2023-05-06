c2a479d1c89ec414b902d81fda17e2eac24a93cd
titi <sackotiti@gmail.com>
2023-05-06 14:08:38 +0100

    async

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,12 +1,39 @@
-const input = process.argv[2]
-const words = input.split(' ')
+import { promisify } from 'util';
+import * as cp from 'child_process';
 
-const result = words.map(word => {
-  const len = Math.ceil(word.length / 2)
-  const firstHalf = word.slice(0, len)
-  const secondHalf = word.slice(len)
-  const reversed = secondHalf + firstHalf
-  return reversed
-}).join('verydisco ').replace(/\p{Emoji_Presentation}/gu, '')
+const exec = promisify(cp.exec);
 
-console.log(result)
+export const tests = [];
+
+tests.push(async ({ path, eq }) => {
+  const { stdout } = await exec(`node ${path} discovery`);
+  return eq(stdout.trim(), 'verydisco 🕺🏼');
+});
+
+tests.push(async ({ path, eq }) => {
+  const { stdout } = await exec(`node ${path} "kiss cool"`);
+  return eq(stdout.trim(), "sski olco");
+});
+
+tests.push(async ({ path, eq }) => {
+  const { stdout } = await exec(`node ${path} kiss cool`);
+  return eq(stdout.trim(), "sski");
+});
+
+tests.push(async ({ path, eq }) => {
+  const { stdout } = await exec(`node ${path} "Node is awesome"`);
+  return eq(stdout.trim(), "deNo si omeawes");
+});
+
+const input = process.argv[2];
+
+const words = input.split(' ');
+
+const discoWords = words.map((word) => {
+  const middleIndex = Math.ceil(word.length / 2);
+  const firstHalf = word.slice(0, middleIndex);
+  const secondHalf = word.slice(middleIndex);
+  return secondHalf + firstHalf;
+});
+
+console.log(discoWords.join('verydisco '));
