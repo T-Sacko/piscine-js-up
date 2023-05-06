bbea174ea7a5495f19c6f3007d91e6128244c9c3
titi <sackotiti@gmail.com>
2023-05-06 12:45:09 +0100

    map

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -4,29 +4,31 @@ document.addEventListener("keydown", function (event) {
 
 function compose(e) {
     if (e === undefined) {
-        return;
+      return;
     }
-    if (
-        [...Array(26).keys()].map((i) => i + 97).includes(e.key.charCodeAt(0))
-    ) {
-        let div = document.createElement("div");
-        div.classList.add("note");
-        div.style.backgroundColor = `rgb(${
-            (255 / 26) * (e.key.charCodeAt(0) - 97)
-        }, ${(255 / 26) * (e.key.charCodeAt(0) - 97)}, ${
-            (255 / 26) * (e.key.charCodeAt(0) - 97)
-        })`;
-        div.innerHTML = e.key;
-        document.body.appendChild(div);
-    } else if (e.key === "Backspace") {
-        let notes = document.getElementsByClassName("note");
-        notes[notes.length - 1].remove();
-    } else if (e.key === "Escape") {
-        let notes = document.getElementsByClassName("note");
-        while (notes.length > 0) {
-            notes[0].remove();
-        }
+    
+    if (e.key === "Escape") {
+      document.querySelectorAll('.note').forEach(note => note.remove())
+      return;
     }
-}
+  
+    if (e.key === "Backspace") {
+      const notes = document.querySelectorAll('.note')
+      if (notes.length > 0) {
+        notes[notes.length - 1].remove()
+      }
+      return;
+    }
+  
+    if (e.key.match(/[a-z]/)) {
+        let div = document.createElement('div')
+        let randomColor = `hsl(${Math.random() * 360}, 100%, 70%)`
+    
+        div.classList.add('note')
+        div.style.backgroundColor = randomColor
+        div.innerHTML = e.key.toUpperCase()
+        document.body.appendChild(div)
+      }
+  }
 
 export { compose };
