1b408500f3220b3d5d9c1487cb54db8a2db1045e
titi <sackotiti@gmail.com>
2023-05-08 01:26:14 +0100

    node

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -4,11 +4,14 @@ import path from 'path';
 const folder = process.argv[2] ? path.resolve(process.argv[2]) : process.cwd();
 
 fs.readdir(folder)
-  .then(async entries => {
-    const names = await Promise.all(entries.map(async entry => {
-      const content = await fs.readFile(path.join(folder, entry), 'utf-8');
-      return content.trim();
-    }));
+  .then(async (entries) => {
+    const names = await Promise.all(
+      entries.map(async (entry) => {
+        const content = await fs.readFile(path.join(folder, entry), 'utf-8');
+        const guest = JSON.parse(content);
+        return `${guest.lastname} ${guest.firstname}`;
+      })
+    );
 
     names.sort();
 
@@ -16,3 +19,6 @@ fs.readdir(folder)
       console.log(`${index + 1}. ${name}`);
     });
   })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
