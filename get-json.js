703fd8678c63d377a835864eb7117cae75ea519a
titi <sackotiti@gmail.com>
2023-05-08 01:35:27 +0100

    node

--- get-json.js
+++ get-json.js
@@ -1,26 +1,25 @@
-async function getJSON(path, params = {}) {
-    // Construct the URL with the path and query parameters
-    const url = new URL(path);
-    url.search = new URLSearchParams(params).toString();
-  
-    // Fetch the data from the URL
-    const response = await fetch(url);
-  
-    // Check if the response is OK, otherwise throw an error with the status text
-    if (!response.ok) {
-      throw new Error(response.statusText);
+async function getJSON(path = '', params = {}) {
+    const url =
+        path +
+        '?' +
+        Object.keys(params)
+            .map((key) => {
+                return (
+                    key.replace(' ', '+') +
+                    '=' +
+                    params[key].toString().replace(' ', '+')
+                );
+            })
+            .join('&');
+    const res = await fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    });
+    if (res.error) {
+        throw new Error(res.error);
     }
-  
-    // Read and parse the JSON response body
-    const jsonResponse = await response.json();
-  
-    // Check if the parsed object contains the 'data' or 'error' property
-    if (jsonResponse.hasOwnProperty('data')) {
-      return jsonResponse.data;
-    } else if (jsonResponse.hasOwnProperty('error')) {
-      throw new Error(jsonResponse.error);
-    } else {
-      throw new Error('Invalid response format');
-    }
-  }
-  
+    return res.data;
+}
