7a1b965359019f5b3720655867a642e110512ade
titi <sackotiti@gmail.com>
2023-05-09 12:52:03 +0100

    node

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -1,47 +1,53 @@
 import http from 'http';
-import fs from 'fs/promises';
+import { join } from 'path';
+import { promises as fs } from 'fs';
 
 const port = 5000;
-const users = {
+const dirName = 'guests';
+const dirPath = join(process.cwd(), dirName);
+const authorizedUsers = {
   'Caleb_Squires': 'abracadabra',
   'Tyrique_Dalton': 'abracadabra',
-  'Rahima_Young': 'abracadabra'
+  'Rahima_Young': 'abracadabra',
 };
 
-async function handleRequest(req, res) {
-  const authorization = req.headers.authorization;
-  const [username, password] = Buffer.from(authorization.split(' ')[1], 'base64').toString().split(':');
-  
-  if (!users[username] || users[username] !== password) {
-    res.writeHead(401, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({error: 'Authorization Required'}));
-    return;
+const server = http.createServer((req, res) => {
+  const { url, method, headers } = req;
+  const [path, auth] = headers.authorization ? headers.authorization.split(' ') : [];
+
+  if (url === '/favicon.ico') {
+    res.writeHead(204);
+    return res.end();
   }
-  
-  if (req.method === 'POST') {
+
+  if (method === 'POST' && authorizedUsers[path.split('/')[1]] === auth) {
     let body = '';
+
     req.on('data', chunk => {
       body += chunk.toString();
     });
+
     req.on('end', async () => {
+      const { answer, drink, food } = JSON.parse(body);
+      const guestName = path.slice(1);
+      const filePath = join(dirPath, `${guestName}.json`);
+
       try {
-        const data = JSON.parse(body);
-        const filename = `${data.name.split(' ').join('_')}.json`;
-        await fs.writeFile(filename, JSON.stringify(data));
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(data));
+        await fs.writeFile(filePath, body);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(body);
       } catch (error) {
-        res.writeHead(500, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({error: 'Server Failed'}));
+        console.error(error);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'server failed' }));
       }
     });
   } else {
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({error: 'Not Found'}));
+    res.writeHead(401, { 'Content-Type': 'application/json', 'WWW-Authenticate': 'Basic realm="Guest List Access"' });
+    res.end(JSON.stringify({ error: 'Unauthorized' }));
   }
-}
+});
 
-const server = http.createServer(handleRequest);
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
